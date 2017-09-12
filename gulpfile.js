7811e8eb8e413eaa7729a36c4ea7119bbda30afc
Varenov Gleb <kuzzdra@gmail.com>
2017-09-12 17:32:51 +0300

    add support for dev- and prod- configs

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const zip = require('gulp-zip');
 const bump = require('gulp-bump');
+// const dotenv = require('dotenv');
+const Q = require('q');
+const template = require('gulp-template');
+const gulpmerge = require('merge2');
+
+var config_loaded = Q.defer();
+var config;
 
 gulp.task('bump', function() {
     gulp.src('src/manifest.json')
@@ -8,15 +15,39 @@ gulp.task('bump', function() {
         .pipe(gulp.dest('src/'));
 });
 
-var zipfunction = function() {
+var buildfunction = function() {
     // берём все файлы и запаковываем
-    gulp.src('src/**')
+
+    // но перед этим в манифест подставляем значения из файлов
+    // конфигурации
+    var manifest = gulp.src('src/manifest.json')
+        .pipe(template({
+            widget_code: config.widget_code,
+            widget_secret_key: config.widget_secret_key,
+        }));
+
+    var other = gulp.src(['src/**', '!src/manifest.json']);
+
+    gulpmerge(manifest, other)
         .pipe(zip('widget.zip'))
-        .pipe(gulp.dest('build/'));
+        .pipe(gulp.dest('./build'));
 };
 
-gulp.task('zip', zipfunction);
+gulp.task('build', ['config'], buildfunction);
 
-gulp.task('bump-build', ['bump'], zipfunction);
+gulp.task('bump-build', ['config', 'bump'], buildfunction);
 
-gulp.task('default', ['bump-build']);
+gulp.task('default', ['config-dev', 'bump-build']);
+
+gulp.task('config', function() {return config_loaded.promise;});
+gulp.task('config-dev', function() {
+    config = require('./config/development.json');
+    config_loaded.resolve();
+});
+gulp.task('config-prod', function() {
+    config = require('./config/production.json');
+    config_loaded.resolve();
+});
+
+gulp.task('dev', ['config-dev', 'build']);
+gulp.task('prod', ['config-prod', 'build']);
