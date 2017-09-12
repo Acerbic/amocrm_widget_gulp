fcd28700d609247920a421c2fbd7d9484a781d40
Varenov Gleb <kuzzdra@gmail.com>
2017-09-12 14:49:02 +0300

    gulp bump functionality

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
 const gulp = require('gulp');
 const zip = require('gulp-zip');
+const bump = require('gulp-bump');
 
-gulp.task('default', function() {
+gulp.task('bump', function() {
+    gulp.src('src/manifest.json')
+        .pipe(bump({key: {widget: 'version'}}))
+        .pipe(gulp.dest('src/'));
+});
+
+var zipfunction = function() {
     // берём все файлы и запаковываем
     gulp.src('src/**')
         .pipe(zip('widget.zip'))
-        .pipe(gulp.dest('build'))
-});
+        .pipe(gulp.dest('build/'));
+};
+
+gulp.task('zip', zipfunction);
+
+gulp.task('bump-build', ['bump'], zipfunction);
+
+gulp.task('default', ['bump-build']);
