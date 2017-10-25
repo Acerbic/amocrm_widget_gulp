fc385045a22ea41ad026ad616f287b4ec53657af
Varenov Gleb <kuzzdra@gmail.com>
2017-10-25 18:31:44 +0300

    refactor tasks invocation

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,54 @@
 const gulp = require('gulp');
-const Q = require('q');
 const runSequence = require('run-sequence');
 const zip = require('gulp-zip');
 const bump = require('gulp-bump');
 const template = require('gulp-template');
 const merge2 = require('merge2');
 
-var config_loaded = Q.defer();
-var config;
+let config;
 
-gulp.task('bump', function() {
-    return gulp.src('src/manifest.json')
+/**
+ * Поднимаем версию виджета в манифесте
+ */
+gulp.task('bump', function () {
+    return gulp.src('./src/manifest.json')
         .pipe(bump({key: {widget: 'version'}}))
-        .pipe(gulp.dest('src/'));
+        .pipe(gulp.dest('./src/'));
 });
 
-gulp.task('build', ['config'], function() {
-    // берём все файлы и запаковываем
+/**
+ * На основании загруженной конфигурации собираем виджет
+ */
+gulp.task('build', ['config'], function () {
 
-    // но перед этим в манифест подставляем значения из файла
-    // конфигурации
-    var manifest = gulp.src('src/manifest.json')
+    // в манифест подставляем значения из конфигурации
+    let manifest = gulp.src('src/manifest.json')
         .pipe(template({
             widget_code: config.widget_code,
             widget_secret_key: config.widget_secret_key,
         }));
 
-    var other = gulp.src(['src/**', '!src/manifest.json']);
+    // Все остальные файлы (кроме манифеста) - просто копируем как есть
+    let other = gulp.src(['src/**', '!src/manifest.json']);
 
+    // Собираем обработанные файлы в один поток, зипуем и сохраняем
     return merge2(manifest, other)
         .pipe(zip('widget.zip'))
         .pipe(gulp.dest('./build'));
 });
 
-gulp.task('bump-build', function (callback) {
-    return runSequence('bump', 'build', callback);
+gulp.task('bump-build', function (done) {
+    return runSequence('bump', 'build', done);
 });
 
-
-gulp.task('config', function() {return config_loaded.promise;});
-gulp.task('config-dev', function() {
-    config = require('./config/development.json');
-    config_loaded.resolve();
+/**
+ * Выбираем конфигурацию сборки из config.json
+ */
+gulp.task('config', function (done) {
+    let configfile = require('./config.json');
+    if (configfile[process.env.NODE_ENV] === undefined) {
+        throw "no config can be loaded for " + process.env.NODE_ENV;
+    }
+    config = configfile[process.env.NODE_ENV];
+    done();
 });
-gulp.task('config-prod', function() {
-    config = require('./config/production.json');
-    config_loaded.resolve();
-});
-
-// повысить версию и собрать в dev конфигурации
-gulp.task('default', ['config-dev', 'bump-build']);
-
-// пересобрать в dev версии
-gulp.task('dev', ['config-dev', 'build']);
-// пересобрать в production версии
-gulp.task('prod', ['config-prod', 'build']);
